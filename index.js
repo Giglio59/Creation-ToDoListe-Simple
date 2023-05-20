11680392462cb817c25d141a59d9ebbf4c112a20
Giglio59 <antoinegiglioo@gmail.com>
2023-05-20 15:24:18 +0200

    Ajoue du bouton sup

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 // Déclaration des variable//
 let input = document.getElementById("TextEntre");
 let bouton = document.getElementById("BoutonValide");
+let boutonSupp = document.getElementById("BoutonSuprimer")
 let liste = document.getElementById("Liste");
+
 
 // fonction de la tache a ajouté //
 
@@ -16,23 +18,32 @@ function TacheAjouter (event){
     input.value = ""
     }
 }
+
+// fonction pour supprimer les tache //
+
+function supprimerTache(event){
+    event.preventDefault();
+    liste.innerHTML="";
+}
 // creation d'écouteur d'evenment //
 
 bouton.addEventListener("click",TacheAjouter);
-
+boutonSupp.addEventListener("click",supprimerTache);
 
 // Explication des tache //
+
 // Je déclare mais variable selon mais ID //
 // Ensuite je créé une fonction pour exectuter mon programe //
 // j'utilise event.preventDefault pour elever le par deffaut du from //
 // je créé une variable pour y mettre la valeur ajouter //
 // Je crée une condition quo dit que si ma valeur ajouter n'est egaal a rien //
-//Alors je créé une variable ou j'y ajoute la reation du baise li//
+// Alors je créé une variable ou j'y ajoute la reation du baise li//
 // J'ajoute ce li dans l'objet document qui et egale a la valeur ajouter //
 // Ensuite j'exectute l'odre d'apprendre cette nouvelle donné//
 // Ensuite je je initialise le input //
 // Et je crée mon ecouteur d'evenment click par rapport a ma fonction //
-
+// je créé une seconde fonction pour supprimer les tache ajouter //
+//Et ensuite son écouteur d'evement clik//
 
 
 
